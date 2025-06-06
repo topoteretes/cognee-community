5ee80fbf8ddf0d89ba416f6df555b4ff4b1204d4
Igor Ilic <igorilic03@gmail.com>
2025-06-06 13:03:59 +0200

    refactor: Add descriptions to azure adapter

--- packages/vector/azureaisearch/example.py
+++ packages/vector/azureaisearch/example.py
@@ -1,30 +1,31 @@
-import sys
+import os
 import asyncio
 import pathlib
 from os import path
 
-######### Needed only for this example, not to be used in production code.
-packages_module_path = path.join(pathlib.Path(__file__).parent.parent.parent.parent)
-sys.path.append(packages_module_path)
-#########
-
+# Please provide an OpenAI API Key
+os.environ["LLM_API_KEY"]=""
 
 async def main():
     from cognee import config, prune, add, cognify, search, SearchType
-    import packages.vector.azureaisearch.register
+    # NOTE: Importing the register module we let cognee know it can use the azure vector adapter
+    from cognee_community_vector_adapter_azure import register
 
     system_path = pathlib.Path(__file__).parent
     config.system_root_directory(path.join(system_path, ".cognee-system"))
     config.data_root_directory(path.join(system_path, ".cognee-data"))
 
-    config.set_relational_db_config({
-        "db_provider": "sqlite",
-    })
+    # Please provide your azure ai search instance url and api key
     config.set_vector_db_config({
         "vector_db_provider": "azureaisearch",
         "vector_db_url": "",
         "vector_db_key": "",
     })
+
+    config.set_relational_db_config({
+        "db_provider": "sqlite",
+    })
+
     config.set_graph_db_config({
         "graph_database_provider": "networkx",
     })
@@ -46,7 +47,7 @@ async def main():
     search_results = await search(query_type=SearchType.GRAPH_COMPLETION, query_text=query_text)
 
     for result_text in search_results:
-        print(result_text)
+        print("\nSearch result: \n" + result_text)
 
 
 if __name__ == "__main__":
