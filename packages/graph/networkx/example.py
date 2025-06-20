3791b1570b84e1ee2e744a644f54bc55788e0b19
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-06-20 16:06:09 +0200

    feat: adds networkx graph db adapter to community adapters

---
+++ packages/graph/networkx/example.py
@@ -0,0 +1,52 @@
+import sys
+import asyncio
+import pathlib
+from os import path
+
+######### Needed only for this example, not to be used in production code.
+packages_module_path = path.join(pathlib.Path(__file__).parent.parent.parent.parent)
+sys.path.append(packages_module_path)
+#########
+
+# NOTE: Importing the register module we let cognee know it can use the Networkx adapter
+import packages.graph.networkx.register
+
+async def main():
+    from cognee import config, prune, add, cognify, search, SearchType
+
+    system_path = pathlib.Path(__file__).parent
+    config.system_root_directory(path.join(system_path, ".cognee-system"))
+    config.data_root_directory(path.join(system_path, ".cognee-data"))
+
+    config.set_relational_db_config({
+        "db_provider": "sqlite",
+    })
+    config.set_vector_db_config({
+        "vector_db_provider": "lancedb",
+    })
+    config.set_graph_db_config({
+        "graph_database_provider": "networkx",
+    })
+
+    await prune.prune_data()
+    await prune.prune_system()
+
+    text = """
+    Natural language processing (NLP) is an interdisciplinary
+    subfield of computer science and information retrieval.
+    """
+
+    await add(text)
+
+    await cognify()
+
+    query_text = "Tell me about NLP"
+
+    search_results = await search(query_type=SearchType.GRAPH_COMPLETION, query_text=query_text)
+
+    for result_text in search_results:
+        print(result_text)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
