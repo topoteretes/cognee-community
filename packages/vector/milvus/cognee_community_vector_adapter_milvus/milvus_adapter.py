c2ef587b0cf77f13450b4f375195699ced1fafb5
Aditi Mondal <aditimondal2013@gmail.com>
2026-06-23 18:51:17 +0530

    Fix: Allow Milvus to create vector embeddings in custom db
    Signed-off-by: Aditi Mondal <aditimondal2013@gmail.com>

--- packages/vector/milvus/cognee_community_vector_adapter_milvus/milvus_adapter.py
+++ packages/vector/milvus/cognee_community_vector_adapter_milvus/milvus_adapter.py
@@ -51,7 +51,7 @@ class MilvusAdapter:
         url: str,
         api_key: str | None,
         embedding_engine: EmbeddingEngine,
-        database_name: str = "cognee",
+        database_name: str = "",
     ):
         self.url = url
         self.api_key = api_key
@@ -91,9 +91,9 @@ class MilvusAdapter:
 
         if not self.client:
             if self.api_key:
-                self.client = MilvusClient(uri=self.url, token=self.api_key)
+                self.client = MilvusClient(uri=self.url, token=self.api_key, db_name=self.database_name)
             else:
-                self.client = MilvusClient(uri=self.url)
+                self.client = MilvusClient(uri=self.url, db_name=self.database_name)
 
         return self.client
 
